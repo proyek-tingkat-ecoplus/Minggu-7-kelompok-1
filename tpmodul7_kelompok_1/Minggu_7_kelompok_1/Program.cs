5759953ab2ce212d014d689e72dd4f5149149993
Dara Sheiba <darasheibaa@gmail.com>
2024-04-01 04:21:12 +0700

    menambahkan <NAMA_CLASS>

--- tpmodul7_kelompok_1/Minggu_7_kelompok_1/Program.cs
+++ tpmodul7_kelompok_1/Minggu_7_kelompok_1/Program.cs
@@ -16,27 +16,27 @@ class Program
 
         // read json file
 
-        string json = File.ReadAllText("C:\\Users\\USER\\Documents\\tpmodul7_kelompok_1\\tpmodul7_kelompok_1\\tp7_1_1302223055.json");
+        string json = File.ReadAllText("D:\\Dara\\KPL\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\tp7_1_1302223075.json");
 
         var options = new JsonSerializerOptions
         {
             IncludeFields = true
         };
-        DataMahasiswa_1302223055 data = JsonSerializer.Deserialize<DataMahasiswa_1302223055>(json, options);
+        DataMahasiswa_1302223075 data = JsonSerializer.Deserialize<DataMahasiswa_1302223075>(json, options);
         data.details();
         //File.WriteAllText("C:\\Users\\USER\\Documents\\Kuliah\\Semester 4\\KPL\\Praktikum\\TPMOD07\\TPMOD07\\TPMOD07\\tp7_1_1302220065.json",);
 
-        string Default_Course = File.ReadAllText("C:\\Users\\USER\\Documents\\tpmodul7_kelompok_1\\tpmodul7_kelompok_1\\tp7_2_1302223055.json");
-        KuliahMahasiswa_1302223055 data2 = JsonSerializer.Deserialize<KuliahMahasiswa_1302223055>(Default_Course, options);
+        string Default_Course = File.ReadAllText("D:\\Dara\\KPL\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\tp7_2_1302223075.json");
+        KuliahMahasiswa_1302223075 data2 = JsonSerializer.Deserialize<KuliahMahasiswa_1302223075>(Default_Course, options);
         data2.PrintCourses();
 
-        string MyCourse = File.ReadAllText("C:\\Users\\USER\\Documents\\tpmodul7_kelompok_1\\tpmodul7_kelompok_1\\matkul.json");
-        KuliahMahasiswa_1302223055 data3 = JsonSerializer.Deserialize<KuliahMahasiswa_1302223055>(MyCourse, options);
+        string MyCourse = File.ReadAllText("D:\\Dara\\KPL\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\matkul.json");
+        KuliahMahasiswa_1302223075 data3 = JsonSerializer.Deserialize<KuliahMahasiswa_1302223075>(MyCourse, options);
         data3.PrintCourses();
         data2.changeToMyCourse(data3);
 
         string new_Course = JsonSerializer.Serialize(data3, options);
-        File.WriteAllText("C:\\Users\\USER\\Documents\\tpmodul7_kelompok_1\\tpmodul7_kelompok_1\\tp7_2_1302223055.json", new_Course);
+        File.WriteAllText("D:\\Dara\\KPL\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\tpmodul7_kelompok1\\matkul.json", new_Course);
 
 
 
